a296fc386efe651017d1974c9eaad22aa8bebcee
tiantiancheng <tiantiancheng@baidu.com>
2025-02-19 13:14:34 +0800

    refactor: 使用vite

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [{
     name: "tpp",
-    script: "serve",  // 使用 serve 命令
-    args: "dist --listen 4173",  // 修改监听配置
+    script: "npx",
+    args: ["vite", "preview", "--host", "0.0.0.0", "--port", "4173"],
     env: {
       NODE_ENV: "production",
       VITE_API_BASE: "http://47.115.231.105"
     }
   }]
-} 
+}
